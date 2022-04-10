d3621522a4581c382c2e937c368862c937cb2f6c
strBruhmomentum <84606621+strBruhmomentum@users.noreply.github.com>
2022-04-10 14:18:33 -0400

    Add files via upload

---
+++ tobey_maguire.py
@@ -0,0 +1,18 @@
+import discord
+import random
+
+class BullyMaguire(discord.Client):
+
+    CMD = "!spider"
+
+    async def on_ready(self):
+        print("Bot ready")
+    
+    async def on_message(self, message: discord.Message):
+        if self.CMD in message.content:
+            with open("spidey_quotes.txt", "r", encoding="utf-8") as f:
+                quotes = f.readlines()
+                await message.channel.send(random.choice(quotes))
+
+client = BullyMaguire()
+client.run("token")
