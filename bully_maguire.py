73828eaaeb6c5d34b41b470ff50dedd75c459ad6
strBruhmomentum <84606621+strBruhmomentum@users.noreply.github.com>
2022-04-10 22:14:09 -0400

    Update bully_maguire.py

--- bully_maguire.py
+++ bully_maguire.py
@@ -4,27 +4,32 @@ from random import choice
 class BullyMaguire(discord.Client):
 
     spidey_keywords = "spider", "peter", "parker", "maguire", "bully"
-    jonah_keywords = "john", "jonah", "jameson", "bugle", "jjj"
+    jameson_keywords = "john", "jonah", "jameson", "bugle", "jjj"
+
+    with open("spidey_quotes.txt", "r", encoding="utf-8") as f:
+        peter_quotes = f.readlines()
+    
+    with open("john_jonah_jameson.txt", "r", encoding="utf-8") as f:
+        jameson_quotes = f.readlines()
 
     async def on_ready(self):
-        print('We have logged in as {}'.format(client.user))
+        print('We have logged in as {}'.format(self.user))
     
     async def on_message(self, message: discord.Message):
+        if self.user.mentioned_in(message):
+            await message.reply("I'm gonna put some dirt in your eye")
+            return
         lower = message.content.lower()
         if message.author != self.user.id:
             for word in self.spidey_keywords:
                 if word in lower:
-                    with open("spidey_quotes.txt", "r", encoding="utf-8") as f:
-                        quotes = f.readlines()
-                        await message.channel.send(choice(quotes))
+                    await message.channel.send(choice(self.peter_quotes))
                     break
-            for word in self.jonah_keywords:
+            for word in self.jameson_keywords:
                 if word in lower:
-                    with open("john_jonah_jameson.txt", "r", encoding="utf-8") as f:
-                        quotes = f.readlines()
-                        await message.channel.send(choice(quotes))
+                    await message.channel.send(choice(self.jameson_quotes))
                     break
 
 
 client = BullyMaguire()
-client.run("token")
+client.run(token")
